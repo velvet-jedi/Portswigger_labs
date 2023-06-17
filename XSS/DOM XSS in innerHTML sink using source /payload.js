6f4672236cc58f0df1d97e856db3cd267e433b10
senior <jedi>
2023-06-17 13:49:20 +0530

    DOM XSS inerHTML sink using location.search

---
+++ XSS/DOM XSS in innerHTML sink using source /payload.js
@@ -0,0 +1,13 @@
+/* 
+function doSearchQuery(query) { 
+    document.getElementById('searchMessage').innerHTML = query; 
+} var query = (new URLSearchParams(window.location.search)).get('search'); 
+if(query) { 
+    doSearchQuery(query); 
+}
+
+<img src=1 onerror=alert(1)>
+
+img tags are self closing
+
+*/
