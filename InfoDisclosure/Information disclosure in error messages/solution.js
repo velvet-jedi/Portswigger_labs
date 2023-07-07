aeb0500e527cd7ac3c25bb528b184703f405b2e1
Pranjal <pranjalruhela2811@gmail.com>
2023-07-07 14:24:14 +0530

    framework, version disclosed by manipulating URL parameter

---
+++ InfoDisclosure/Information disclosure in error messages/solution.js
@@ -0,0 +1,7 @@
+/*
+    https://0ac900e703d70ee580e5d16e003500cd.web-security-academy.net/product?productId=1
+
+    replace with
+
+    https://0ac900e703d70ee580e5d16e003500cd.web-security-academy.net/product?productId=ss
+ */
