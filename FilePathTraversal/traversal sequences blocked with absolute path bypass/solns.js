2469af04dfcbf774069ea28dfb59d5cb07e14b01
Pranjal <pranjalruhela2811@gmail.com>
2023-07-08 10:10:48 +0530

    ./ (trav seq) blocked but absolute referencing in pathname

---
+++ FilePathTraversal/traversal sequences blocked with absolute path bypass/solns.js
@@ -0,0 +1,9 @@
+/*
+
+The application blocks traversal sequences 
+but treats the supplied filename as being relative to a default working directory
+(the root directory possibly)
+
+https://0ad7006604a0828a8448130500f300a2.web-security-academy.net/image?filename=/etc/passwd
+
+*/
