a362a1c486b0c87e2baf32a5f801741f19863747
Pranjal <pranjalruhela2811@gmail.com>
2023-07-08 15:02:38 +0530

    failed 'security by obscurity'

---
+++ BrokenAccessControl/Unprotected admin functionality with unpredictable URL/labsol.js
@@ -0,0 +1,15 @@
+/*
+page source reveals
+var isAdmin = false;
+if (isAdmin) {
+   var topLinksTag = document.getElementsByClassName("top-links")[0];
+   var adminPanelTag = document.createElement('a');
+   adminPanelTag.setAttribute('href', '/admin-crls3w');
+   adminPanelTag.innerText = 'Admin panel';
+   topLinksTag.append(adminPanelTag);
+   var pTag = document.createElement('p');
+   pTag.innerText = '|';
+   topLinksTag.appendChild(pTag);
+}
+
+*/
