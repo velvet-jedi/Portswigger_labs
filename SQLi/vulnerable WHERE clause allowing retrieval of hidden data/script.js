9745dc8101d5318371bb9e4eb1e8c1e4610af7b8
Pranjal <pranjalruhela2811@gmail.com>
2023-07-24 20:56:42 +0530

    URL trickery

---
+++ SQLi/vulnerable WHERE clause allowing retrieval of hidden data/script.js
@@ -0,0 +1,8 @@
+/*
+
+when user selects the Gifts category and filters the results
+SELECT * FROM products WHERE category = 'Gifts' AND released = 1
+
+enter this in the URL bar
+Gifts'+OR+1=1--
+*/
