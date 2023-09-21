7705271ace390f008eeaf3fb7b3e884ef0a2d8f4
senior <jedi>
2023-09-21 22:35:53 +0530

    JWK headr injection, arbitrary signature with embedded key manipulation

---
+++ JWT/JWT authentication bypass via jwk header injection/adsfwer.js
@@ -0,0 +1,19 @@
+/*
+
+1. if the site allows jwk header injection, and doesnt check the source
+ 
+2. go to the JWT Editor Keys tab. Generate a new RSA key.
+
+3. Send a request containing JWT to Repeater.
+
+4. In the message editor, switch to the extension-generated JSON Web Token tab 
+   and modify the token's payload however you like.
+
+5. Click Attack, then select Embedded JWK. When prompted, select your 
+   newly generated RSA key.
+
+6. Send the request
+
+
+
+*/
